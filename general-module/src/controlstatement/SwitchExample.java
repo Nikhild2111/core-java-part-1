c406832ea33d76f6a2cfe2221651fde5d32d9d7a
sdabhade <sahildabhade21@gmail.com>
2023-06-16 17:23:09 +0530

    commit and push SwitchExample.java

--- general-module/src/controlstatement/SwitchExample.java
+++ general-module/src/controlstatement/SwitchExample.java
@@ -1,4 +1,14 @@
 package controlstatement;
 
+import java.util.Scanner;
+
 public class SwitchExample {
+    public static void main(String[] args) {
+        System.out.println("Please enter a string : ");
+        //User input
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println("User has entered : "+input);
+
+    }
 }
