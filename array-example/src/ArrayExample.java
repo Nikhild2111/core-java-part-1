e51be6ff985487909d17afa9e3d06432fa0c20f0
sdabhade <sahildabhade21@gmail.com>
2023-06-17 16:28:29 +0530

    commit and push ArrayExample and MenuDrivenExample

---
+++ array-example/src/ArrayExample.java
@@ -0,0 +1,20 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        //even number array till 10 -> {0,2,4,6,8,10}
+        //prime number till 10-> {2,3,5,7} //array indexing starts with 0.
+
+        int[] primeNums = new int[] {2,3,5,7};
+
+        for ( int num : primeNums) {
+            System.out.println(num);
+        }
+
+
+        String[] fruits = new String[] {"Apple", "Mango", "Orange", "Pomegranate", "Grapes"};
+
+        for (String nameofFruit : fruits) {
+            System.out.println(nameofFruit);
+
+        }
+    }
+}
